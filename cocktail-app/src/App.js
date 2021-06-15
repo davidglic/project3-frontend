c0d149681c3c4205c9d5713225247390729de806
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-06-15 16:54:07 -0500

    landing created

--- cocktail-app/src/App.js
+++ cocktail-app/src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import {Link, Route} from "react-router-dom";
 import Header from './components/Header';
 import Signup from './components/Signup';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Route
+        path="/"
+        exact render={() => <Landing/>}
+      />
       <Route
         path ="/signup"
         render={() => <Signup/>}
---
+++ cocktail-app/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import axios from "axios";
+
+const Landing = ()=> {
+    return(
+        <div>
+            <h1> Drinks Array:</h1>
+
+        </div>
+    )
+}
+export default Landing;
