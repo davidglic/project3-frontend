8c73b89404219042654151524a0bd1ad9bc1fb69
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-06-16 14:56:24 -0500

    drink info added

--- cocktail-app/src/components/Drink.js
+++ cocktail-app/src/components/Drink.js
@@ -29,13 +29,99 @@ class Drink extends Component {
     render () {
         console.log(this.state.drink)
     return (
+    <div>
+        {this.state.recievedDrink &&
         <div>
-            {this.state.recievedDrink &&
-            <div>
-                {this.state.drink.strDrink} <br></br>
-                {this.state.drink.strIngredient1}
-                {this.state.drink.strIngredient4}
-
+            <h1>{this.state.drink.strDrink} </h1> <br></br>
+            <img src={this.state.drink.strDrinkThumb} height="300px"/>
+        <div className="container">    
+            <div className="ingredientContainer">
+                <h3>Glass:</h3>
+                <p className="ingredients">{this.state.drink.strGlass}</p>
+                <h3>Ingredients:</h3>
+                <ul className="ingredients"> 
+                    {this.state.drink.strIngredient1 != null &&
+                    <li>
+                         {this.state.drink.strMeasure1} {this.state.drink.strIngredient1}                        
+                    </li>
+                    }
+                    {this.state.drink.strIngredient2 != null &&
+                    <li>
+                        {this.state.drink.strMeasure2} {this.state.drink.strIngredient2}                        
+                    </li>
+                    }
+                    {this.state.drink.strIngredient3 != null &&
+                    <li>
+                       {this.state.drink.strMeasure3} {this.state.drink.strIngredient3}                         
+                    </li>
+                    }
+                    {this.state.drink.strIngredient4 != null &&
+                    <li>                        
+                        {this.state.drink.strMeasure4} {this.state.drink.strIngredient4} 
+                    </li>
+                    }
+                    {this.state.drink.strIngredient5 != null &&
+                    <li>                        
+                        {this.state.drink.strMeasure5} {this.state.drink.strIngredient5} 
+                    </li>
+                    }
+                    {this.state.drink.strIngredient6 != null &&
+                    <li>                         
+                        {this.state.drink.strMeasure6} {this.state.drink.strIngredient6}
+                    </li>
+                    }
+                    {this.state.drink.strIngredient7 != null &&
+                    <li>                        
+                        {this.state.drink.strMeasure7} {this.state.drink.strIngredient7} 
+                    </li>
+                    }
+                    {this.state.drink.strIngredient8 != null &&
+                    <li>                        
+                        {this.state.drink.strMeasure8} {this.state.drink.strIngredient8} 
+                    </li>
+                    }
+                    {this.state.drink.strIngredient9 != null &&
+                    <li>                         
+                        {this.state.drink.strMeasure9} {this.state.drink.strIngredient9}
+                    </li>
+                    }
+                    {this.state.drink.strIngredient10 != null &&
+                    <li>                         
+                        {this.state.drink.strMeasure10} {this.state.drink.strIngredient10}
+                    </li>
+                    }
+                    {this.state.drink.strIngredient11 != null &&
+                    <li>                        
+                        {this.state.drink.strMeasure11} {this.state.drink.strIngredient11} 
+                    </li>
+                    }
+                    {this.state.drink.strIngredient12 != null &&
+                    <li>                         
+                        {this.state.drink.strMeasure12} {this.state.drink.strIngredient12}
+                    </li>
+                    }
+                    {this.state.drink.strIngredient13 != null &&
+                    <li>                        
+                        {this.state.drink.strMeasure13} {this.state.drink.strIngredient13} 
+                    </li>
+                    }
+                    {this.state.drink.strIngredient14 != null &&
+                    <li>                        
+                        {this.state.drink.strMeasure14} {this.state.drink.strIngredient14} 
+                    </li>
+                    }
+                    {this.state.drink.strIngredient15 != null &&
+                    <li>
+                        {this.state.drink.strMeasure15} {this.state.drink.strIngredient15} 
+                    </li>
+                    }
+                </ul> 
+            </div>
+            <div className="instructionContainer">
+                <h3>Instructions:</h3>
+                <p className="instructions">{this.state.drink.strInstructions}</p>
+            </div> 
+        </div> 
             </div>
             }
         </div>
